85c30ae852123b7656ddc4f6863210efdc58768c
dir-FY24oshimayui <yui.oshima.z029@dirbato.co.jp>
2024-04-08 16:05:40 +0900

    sample

---
+++ f/Sample/src/sample/Variable.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+       var number=1;
+       System.out.println(number);
+       
+       String str1;
+       str1="abc";
+       System.out.println(str1);
+               
+       
+
+    }
+
+}
